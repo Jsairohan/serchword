a547736cf852fba097564ecabd3f1286071f6673
Jsairohan <sairohan@users.noreply.github.com>
2019-08-11 20:16:30 +0530

    sds

--- searchtext/urls.py
+++ searchtext/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'^home/',views.index),
+    url(r'^',views.index),
     url(r'^search/$', views.getsearchdataview.as_view(),name='wordtext'),
 
 ]
