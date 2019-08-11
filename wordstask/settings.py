7a4a3b5d80591ea134b4a0e95981e26b6539c2af
Jsairohan <sairohan@users.noreply.github.com>
2019-08-11 17:31:25 +0530

    junk

--- wordstask/settings.py
+++ wordstask/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_heroku
-
+#
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -134,6 +134,6 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 
-sSTATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
